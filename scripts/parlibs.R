6245f37835da4f7166453d25b99d1d460bc94152
Lukas <lukas.arnroth@gmail.com>
2024-04-15 12:46:11 +0200

    for parallel computation

---
+++ scripts/parlibs.R
@@ -0,0 +1,17 @@
+suppressPackageStartupMessages(library(INLA))
+#inla.setOption(inla.timeout = 10*60)
+inla.setOption(inla.timeout = 3*60*60)
+source("./scripts/SPOTS/helpers.R")
+source("./scripts/Highplex/helpers.R")
+source("./INLA/LCAR.R")
+source("./INLA/MCAR.R")
+source("./INLA/CCAR.R")
+source("./INLA/MCCAR.R")
+source("./INLA/spotCCAR.R")
+source("./INLA/spotMCCAR.R")
+
+quiet <- function(x) { 
+  sink(tempfile()) 
+  on.exit(sink()) 
+  invisible(force(x)) 
+} 
